7ab2b314c4afde396a96da7a9359fdf33d92a195
Jip <e1r1b@ugrad.cs.ubc.ca>
2019-02-14 06:09:01 -0800

    l

---
+++ src/Local Pages/blocked-sites.js
@@ -0,0 +1,80 @@
+let urlsToBlock;
+const BLOCKED_SITE_STORAGE = 'blockedSites';
+
+let listOfSites = document.getElementById("messageListId");
+
+chrome.storage.sync.get([BLOCKED_SITE_STORAGE], (blockedSites) => {
+    if(blockedSites[BLOCKED_SITE_STORAGE]) {
+        urlsToBlock = blockedSites[BLOCKED_SITE_STORAGE];
+        console.log(urlsToBlock);
+        if (urlsToBlock.length > 0) {
+            document.getElementById('noBlock').remove();
+            urlsToBlock.forEach((url) => {addEntry(url, true)});
+        } 
+    }
+})
+
+function addEntry(url, isAlreadyRegex) {
+    let newMessageEntry = document.createElement("li");
+    let messageText = document.createElement("p");
+    
+    console.log(isAlreadyRegex);
+    messageText.className = "li-message";
+    
+    // Get just domain name (facebook.com)
+    if (isAlreadyRegex) {
+        url = url.substring(url.indexOf('.') + 1, url.length-2);
+    }
+
+    messageText.innerText = url;
+    newMessageEntry.appendChild(messageText);
+    listOfSites.appendChild(newMessageEntry);
+    messageText.addEventListener('click', () => {
+        selectEntry(messageText);
+    });
+}
+
+document.getElementById('website-button').addEventListener('click', () => {
+    try {
+        let urlEntered = document.getElementById("website-input").value;
+        document.getElementById('website-input').value = "";
+        // testing if valid url
+        new URL(urlEntered);
+        let tmp = document.createElement('a');
+        tmp.href = urlEntered;
+        urlsToBlock.push("*://*." + tmp.hostname + "/*");
+        if (document.getElementById('noBlock')) {
+            document.getElementById('noBlock').remove();
+        }
+        addEntry(tmp.hostname, false);
+        setBlockStorage();
+    } catch(e) {
+        console.log(e);
+        alert('not a valid website, please try again');
+    }
+});
+
+document.getElementById('remove-button').addEventListener('click', () => {
+    let selectedEntries = document.getElementsByClassName('selected');
+    for (let i = 0; i < selectedEntries.length; i++) {
+        urlsToBlock.splice(urlsToBlock.indexOf("*://*." + selectedEntries[i].innerText + "/*"), 1);
+        selectedEntries[i].remove();
+    }
+    setBlockStorage();
+});
+
+function setBlockStorage() {
+    chrome.storage.sync.set({[BLOCKED_SITE_STORAGE]:urlsToBlock}, () => {
+        chrome.storage.sync.get([BLOCKED_SITE_STORAGE], (data) => {
+            console.log(data[BLOCKED_SITE_STORAGE]);
+        });
+    });
+}
+
+function selectEntry(entry) {
+    if (entry.classList.contains('selected')) {
+        entry.classList.remove('selected');
+    } else {
+        entry.classList.add('selected');
+    }
+}
--- src/Local Pages/blocked.js
+++ src/Local Pages/blocked.js
@@ -1,14 +1,28 @@
-import {storage, BLOCKED_ALARM_TIME_STORAGE, BLOCKED_STATUS, BREAK_ALARM_TIME_STORAGE, cuteMessageIdentifier} from "../Background/bgd.js";
-import { getImageToUse, showNotification} from "./options.js";
-import {PROGRAM_STATE} from "../Background/popup.js";
+import { storage, BLOCKED_ALARM_TIME_STORAGE, BLOCKED_STATUS, BREAK_ALARM_TIME_STORAGE, cuteMessageIdentifier } from "../Background/bgd.js";
+import { getImageToUse, showNotification } from "./options.js";
+import { PROGRAM_STATE } from "../Background/popup.js";
 
-console.log(document);
 let timeFromNow;
 let button = document.getElementById("demo");
 
-document.getElementById("lol").src = getImageToUse();
+if (document.getElementById("lol")) {
+    document.getElementById("lol").src = getImageToUse();
+}
 
-chrome.runtime.onMessage.addListener(function(message) {
+startTimer(button);
+
+function startTimer(timer) {
+    storage.get(PROGRAM_STATE, function (data) {
+        if (data.programState) {
+            getParametersToStartTimer(timer);
+        } else {
+            timer.innerText = "Program isn't active";
+        }
+    })
+}
+
+
+chrome.runtime.onMessage.addListener(function (message) {
     let messages = message.split(cuteMessageIdentifier);
 
     if (messages.length > 1) {
@@ -16,54 +30,39 @@ chrome.runtime.onMessage.addListener(function(message) {
     }
 });
 
-button.addEventListener("click", function () {
-    storage.get(PROGRAM_STATE, function(data) {
-       if (data.programState) {
-           getParametersToStartTimer();
-       } else {
-           button.innerText = "Program isn't active";
-       }
-    });
-});
-
-function getParametersToStartTimer() {
+function getParametersToStartTimer(timer) {
     storage.get(BLOCKED_STATUS, function (data) {
         let isBlocked = data[BLOCKED_STATUS];
         if (data.blocked) {
             storage.get(BLOCKED_ALARM_TIME_STORAGE, function (data) {
-                startCountdown(data[BLOCKED_ALARM_TIME_STORAGE], isBlocked);
+                startCountdown(data[BLOCKED_ALARM_TIME_STORAGE], isBlocked, timer);
             });
         }
         else {
             storage.get(BREAK_ALARM_TIME_STORAGE, function (data) {
-                startCountdown(data[BREAK_ALARM_TIME_STORAGE], isBlocked);
+                startCountdown(data[BREAK_ALARM_TIME_STORAGE], isBlocked, timer);
             });
         }
     });
 }
 
-function startCountdown(timeOfAlarm, isBlocked) {
+function startCountdown(timeOfAlarm, isBlocked, timer) {
     timeFromNow = timeOfAlarm;
     let toCountdownTo = new Date(timeFromNow);
     let captions = getCaptions(isBlocked);
 
-    controlTimer(toCountdownTo, captions);
+    controlTimer(toCountdownTo, captions, timer);
 }
 
 
-function showClickAgainMessageIfNeeded(captions) {
-    if (button.innerText === captions[1]) {
-        button.innerText = captions[2];
+function showClickAgainMessageIfNeeded(captions, timer) {
+    if (timer.innerText === captions[1]) {
+        timer.innerText = captions[2];
     }
 }
 
-function controlTimer(toCountdownTo, captions) {
-    let timerInterval = setInterval(function () {
-
-        button.addEventListener('click', function () {
-            clearInterval(timerInterval);
-        });
-
+function controlTimer(toCountdownTo, captions, timer) {
+    let timerInterval = setInterval(() => {
         chrome.storage.onChanged.addListener(function (changes, namespace) {
             console.log(changes);
             if (changes[BLOCKED_ALARM_TIME_STORAGE]) {
@@ -78,12 +77,14 @@ function controlTimer(toCountdownTo, captions) {
         let seconds = Math.floor((distance / 1000) % 60);
         let minutes = Math.floor((distance / 1000 / 60) % 60);
 
-        button.innerText = captions[0] + minutes + " min " + seconds + " sec";
+        if (timer){
+            timer.innerText = captions[0] + minutes + " min " + seconds + " sec";
+        }
 
-        if (distance < 0) {
+        if (distance < 0 && timer) {
             clearInterval(timerInterval);
-            button.innerText = captions[1];
-            setTimeout(function() {showClickAgainMessageIfNeeded(captions)}, 5000);
+            timer.innerText = captions[1];
+            setTimeout(function () { showClickAgainMessageIfNeeded(captions) }, 5000);
         }
     }, 1000);
 }
@@ -91,9 +92,9 @@ function controlTimer(toCountdownTo, captions) {
 function getCaptions(isBlocked) {
     if (isBlocked) {
         return ["Time Until Break: ", "Go take your well-deserved break",
-            "Click this button again to show break timer"];
+            "Click this timer again to show break timer"];
     } else {
         return ["Time Until Break's Over: ", "Time to go back to work!",
-            "Click this button again to show block timer"];
+            "Click this timer again to show block timer"];
     }
 }
